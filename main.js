bdffa6b9a5123b2937f8de4ea4a6326f3e6f8930
Dhannveen Uppal <78311201+DhannveenUppal7@users.noreply.github.com>
2022-06-02 21:06:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBoqGwhAgXeFAfJ7fBXbPSI-lQuuaL7hKU",
-    authDomain: "dwit-b8bad.firebaseapp.com",
-    projectId: "dwit-b8bad",
-    storageBucket: "dwit-b8bad.appspot.com",
-    messagingSenderId: "886471747508",
-    appId: "1:886471747508:web:7aa8c5c405817b93fbb4e1"
+    // Firebase Config
 };
 firebase.initializeApp(firebaseConfig);
 
